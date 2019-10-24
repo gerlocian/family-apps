2b76cd98ba181bef6c9136a1a71b64c3c84db875
pogithub@gmail.com <pogithub@gmail.com>
2019-10-24 00:50:38 -0400

    Working on API Gateway to Step Function interface

---
+++ workflows/isbn-lookup/isbn-lookup.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  profile = "portiz"
+  region  = "us-east-1"
+}
+
+data "aws_iam_role" "step_function_lambda_execution" {
+  name = "StepFunctionsLambdaExecution"
+}
+
+data "aws_lambda_function" "isbn_lookup" {
+  function_name = yamldecode(file("../../services/isbn-lookup/isbn-lookup.yml")).functionName
+}
+
+data "aws_lambda_function" "isbn_map" {
+  function_name = yamldecode(file("../../services/isbn-map/isbn-map.yml")).functionName
+}
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name  = "isbn-lookup-state-machine"
+  role_arn  = data.aws_iam_role.step_function_lambda_execution.arn
+
+  definition = <<EOF
+    {
+      "Comment": "Application state steps for ISBN Lookup",
+      "StartAt": "Get ISBN Results",
+      "States": {
+        "Get ISBN Results": {
+          "Type": "Task",
+          "Resource": "${data.aws_lambda_function.isbn_lookup.arn}",
+          "Next": "Map Results"
+        },
+        "Map Results": {
+          "Type": "Task",
+          "Resource": "${data.aws_lambda_function.isbn_map.arn}",
+          "End": true
+        }
+      }
+    }
+  EOF
+}
+
+terraform {
+  backend "s3" {
+    profile = "portiz"
+    region  = "us-east-1"
+    bucket  = "family-apps-state-bucket"
+    key     = "app-steps"
+  }
+}
